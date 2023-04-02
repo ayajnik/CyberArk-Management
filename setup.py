d1c4379d86511e192629726ee1d9a0bc135c6a56
Ayush Yajnik <46913656+ayajnik@users.noreply.github.com>
2023-04-02 15:26:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setuptools.setup(
     "click==8.1.3",
     "colorama==0.4.6",
     "Cython==0.29.33",
-    "distlib==0.3.4"
+    "distlib==0.3.4",
     "filelock==3.7.0",
     "Flask==2.2.2",
-    "Flask-Login==0.6.2"
+    "Flask-Login==0.6.2",
     "Flask-Migrate==4.0.2",
     "Flask-SQLAlchemy==3.0.2",
     "Flask-WTF==1.1.1",
@@ -34,8 +34,8 @@ setuptools.setup(
     "Jinja2==3.1.2",
     "Mako==1.2.4",
     "MarkupSafe==2.1.2",
-    "numpy==1.24.2"
-    "platformdirs==2.5.2"
+    "numpy==1.24.2",
+    "platformdirs==2.5.2",
     "python-dotenv==0.21.1",
     "requests==2.28.2",
     "pandas==1.5.3"
